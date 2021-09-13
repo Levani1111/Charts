5e0ff7904930d14e5d9c11118224c6b4fe8cc15d
Your Name <levani.papashvili@labshare.org>
2021-09-13 16:31:10 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { HeatMapChartComponent } from './heat-map-chart/heat-map-chart.component';
+import { GaugeChartComponent } from './gauge-chart/gauge-chart.component';
+import { PolarRadarChartComponent } from './polar-radar-chart/polar-radar-chart.component';
+import { LinearGaugeChartComponent } from './linear-gauge-chart/linear-gauge-chart.component';
+import { NumberCardChartComponent } from './number-card-chart/number-card-chart.component';
+import { TreeMapChartComponent } from './tree-map-chart/tree-map-chart.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
+import { ProductBarChartsComponent } from './product-bar-charts/product-bar-charts.component';
+import { ProductPieChartsComponent } from './product-pie-charts/product-pie-charts.component';
+import { ProductAreaLineChartsComponent } from './product-area-line-charts/product-area-line-charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeatMapChartComponent,
+    GaugeChartComponent,
+    PolarRadarChartComponent,
+    LinearGaugeChartComponent,
+    NumberCardChartComponent,
+    TreeMapChartComponent,
+    BubbleChartComponent,
+    ProductBarChartsComponent,
+    ProductPieChartsComponent,
+    ProductAreaLineChartsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
